e22833c31a69145bec76933a9076ea8c31ea21d5
Phil Cooper-King <phil@cooperking.net>
2016-07-05 11:48:25 +0100

    Have rot working.

--- code/main.cpp
+++ code/main.cpp
@@ -18,7 +18,9 @@
 int
 main()
 {
-  Core::Context context(800, 480, false, "FPS Test");
+  Core::Context_setup setup;
+  setup.vsync = true;
+  Core::Context context(800, 480, false, "FPS Test", setup);
   Core::World world(context);
   
   // ** RESOURCES ** //
@@ -46,8 +48,9 @@ main()
     main_camera.set_attached_entity(cam_entity);
     main_camera.set_priority(1);
     
-    main_camera.set_width(800);
-    main_camera.set_height(480);
+    main_camera.set_width(context.get_width());
+    main_camera.set_height(context.get_height());
+    main_camera.set_feild_of_view(math::quart_tau() * 0.5f);
   }
   
   Core::Entity ground_entity(world);
@@ -72,20 +75,38 @@ main()
     // FPS Input
     {
       Core::Input::Controller controller(context, 0);
+      Core::Transform trans = cam_entity.get_transform();
       
       // Move Axis
       {
-        Core::Transform trans = cam_entity.get_transform();
-        
-        const math::vec3 fwd = math::vec3_scale(trans.get_forward(), controller.get_axis(0).y * world.get_delta_time());
-        const math::vec3 left = math::vec3_scale(trans.get_left(), controller.get_axis(0).x * world.get_delta_time());
+        const float move_mul = world.get_delta_time() * 4.f;
+      
+        const math::vec3 fwd      = math::vec3_scale(trans.get_forward(), controller.get_axis(0).y * move_mul);
+        const math::vec3 left     = math::vec3_scale(trans.get_left(), controller.get_axis(0).x * move_mul);
         const math::vec3 curr_pos = trans.get_position();
-        const math::vec3 new_pos = math::vec3_add(math::vec3_add(curr_pos, fwd), left);
+        const math::vec3 new_pos  = math::vec3_add(math::vec3_add(curr_pos, fwd), left);
         
         trans.set_position(new_pos);
-        
-        cam_entity.set_transform(trans);
       }
+      
+      // Rot Axis
+      {
+        const float rot_mul = world.get_delta_time() * 3.f;
+      
+        static float accum_x = 0.f;
+        static float accum_y = 0.f;
+        
+        accum_x += controller.get_axis(1).x * rot_mul;
+        accum_y += controller.get_axis(1).y * rot_mul;
+        
+        const math::quat yaw   = math::quat_init_with_axis_angle(0, 1, 0, accum_x);
+        const math::quat pitch = math::quat_init_with_axis_angle(1, 0, 0, accum_y);
+        const math::quat rot   = math::quat_multiply(pitch, yaw);
+                
+        trans.set_rotation(rot);
+      }
+      
+      cam_entity.set_transform(trans);
     }
   
     world.think();
